bf4b83596276988cb5cbe9e595545e41bfe32fd9
Bryan Hansen <bryankhansen@gmail.com>
2025-04-28 18:10:35 -0600

    Module 1 start state

--- scripts/first_script.py
+++ scripts/first_script.py
@@ -10,29 +10,7 @@ options.add_argument("--log-level=3")  # Suppress console warnings (INFO, WARNIN
 driver = webdriver.Chrome(options=options)
 
 try:
-    # Load the login page
-    driver.maximize_window()
-    driver.get("https://selenium.completeprogrammer.com/login.html")
     
-    # Fill in username and password
-    driver.find_element(By.ID, "username").send_keys("jack")
-    driver.find_element(By.ID, "password").send_keys("test1234")
-
-    # Submit the form
-    driver.find_element(By.ID, "loginButton").click()
-
-    # Wait for result message to appear
-    # Wait for the loginMessage element to become visible
-    message_element = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.ID, "loginMessage"))
-    )
-
-     #Get the message text and check for "Login successful!"
-    message_text = message_element.text
-    assert "Login successful!" in message_text, f"Login failed! Message: {message_text}"
-
-    print("✅ Login was successful and confirmed via result message.")
-
 finally:
     time.sleep(2)
     driver.quit()
